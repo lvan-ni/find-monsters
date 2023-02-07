fee2e6b24c082008e20efd3e6b739486b58c910f
gnilvan <gnilvan@users.noreply.github.com>
2023-02-07 16:24:01 +0100

    converted all class components into functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
@@ -6,6 +6,20 @@ import './App.css';
 const App = () => {
 
   const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, [])
+
+  useEffect(() => {
+    setFilteredMonsters(monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    }));
+  }, [monsters, searchField])
 
   const onSearchChange = (e) => {
     const searchFieldString = e.target.value.toLocaleLowerCase();
@@ -82,4 +96,4 @@ const App = () => {
 // }
 
 
-// export default App;
+export default App;
--- src/components/search-box/search-box.component.js
+++ src/components/search-box/search-box.component.js
@@ -1,18 +1,12 @@
-import { Component } from "react";
 import './search-box.style.css';
 
-class SearchBox extends Component {
-
-    render() {
-        return (
-            <input 
-             className={`search-box ${this.props.className}`}
-             type='search' 
-             placeholder={this.props.placeholder}
-             onChange={this.props.onChange}
-            />
-        )
-    }
-} 
+const SearchBox = ({className, placeholder, onChange}) => (
+    <input 
+      className={`search-box ${className}`}
+      type='search' 
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+)
 
 export default SearchBox;
